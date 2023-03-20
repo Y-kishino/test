37052493885bb1f51d8fa7874f24e8cc279ec90c
Y-kishino <kishino0815@gmail.com>
2023-03-20 23:20:22 +0900

    JSの追加

---
+++ foo.js
@@ -0,0 +1,67 @@
+let youtuber = {
+    list:{
+        business:{
+            youtuber:[{
+                name:kishino,
+                age:25,
+                sex:man,
+                teachIT:function name(params) {    
+                }
+            },
+            {
+                name:tanaka,
+                age:20,
+                sex:woman,
+                teachEnglish:function name(params) {
+                    
+                }
+            },
+            {
+                name:suzuki,
+                age:30,
+                sex:man,
+                teachJapanese:function name(params) {
+                    
+                }
+            }
+        ],
+        teach(){
+
+        }
+        },
+        entertaiment:{
+            youtuber:[{
+                name:hikakinn,
+                age:35,
+                sex:man,
+                teach:function name(params) {
+                    
+                }
+            },
+            {
+                name:bannbanzai,
+                age:21,
+                sex:man,
+                teach:function name(params) {
+                    
+                }
+            },
+            {
+                name:yuuri,
+                age:28,
+                sex:man,
+                teachMusic:function name(params) {
+                    
+                }
+            }
+        ]}
+    },
+    plan:function(){
+        
+    },
+    uploadVideo:function () {
+        
+    }
+}
+
+export default youtuber;
